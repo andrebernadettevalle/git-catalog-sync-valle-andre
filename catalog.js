c2c22370a8e26c00a3479e39e3006e5e3b5c5136
andrebernadettevalle <valleandre05@gmail.com>
2026-09-23 16:03:54 +0800

    Include missing 1-day grace period in final release - valle.andre

--- catalog.js
+++ catalog.js
@@ -3,16 +3,24 @@ function isValidLoan(daysLate) {
 }
 
 function calculateLateFee(daysLate, ratePerDay) {
+  // Rule 1: 1-Day Grace Period
   if (daysLate <= 1) {
     return 0;
   }
+
+  // Rule 2: Fee Rounding
   let fee = Math.round(daysLate * ratePerDay);
+
+  // Rule 3: $1 Minimum Fee
   if (fee > 0 && fee < 1) {
     fee = 1;
   }
+
+  // Rule 4: $20 Maximum Cap
   if (fee > 20) {
     fee = 20;
   }
+
   return fee;
 }
 
