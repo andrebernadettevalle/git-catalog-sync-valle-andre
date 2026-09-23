2b83dcf6a3dda8679c9ab2a6790bf8f6f87cb1cb
andrebernadettevalle <valleandre05@gmail.com>
2026-09-23 15:59:14 +0800

    Resolve rebase conflict combining all fee rules - valle.andre

--- catalog.js
+++ catalog.js
@@ -3,18 +3,17 @@ function isValidLoan(daysLate) {
 }
 
 function calculateLateFee(daysLate, ratePerDay) {
-<<<<<<< HEAD
   if (daysLate <= 1) {
     return 0;
   }
   let fee = Math.round(daysLate * ratePerDay);
+  if (fee > 0 && fee < 1) {
+    fee = 1;
+  }
   if (fee > 20) {
     fee = 20;
   }
   return fee;
-=======
-  return Math.round(daysLate * ratePerDay);
->>>>>>> 34889dd (Add  minimum fee for late returns - valle.andre)
 }
 
 module.exports = { isValidLoan, calculateLateFee };
