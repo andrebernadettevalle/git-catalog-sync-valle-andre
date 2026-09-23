e44d84da43719ad00f0333efeffb90920bec4445
andrebernadettevalle <valleandre05@gmail.com>
2026-09-23 15:52:31 +0800

    Add  minimum fee for late returns - valle.andre

--- catalog.js
+++ catalog.js
@@ -3,6 +3,7 @@ function isValidLoan(daysLate) {
 }
 
 function calculateLateFee(daysLate, ratePerDay) {
+<<<<<<< HEAD
   if (daysLate <= 1) {
     return 0;
   }
@@ -11,6 +12,9 @@ function calculateLateFee(daysLate, ratePerDay) {
     fee = 20;
   }
   return fee;
+=======
+  return Math.round(daysLate * ratePerDay);
+>>>>>>> 34889dd (Add  minimum fee for late returns - valle.andre)
 }
 
 module.exports = { isValidLoan, calculateLateFee };
